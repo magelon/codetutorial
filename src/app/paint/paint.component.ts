c78fcd6abad23736365ed26021ae9250ac2b088d
magelon <alfardproject@gmail.com>
2020-04-09 23:00:04 -0700

    c

--- src/app/paint/paint.component.ts
+++ src/app/paint/paint.component.ts
@@ -17,14 +17,32 @@ export class PaintComponent implements OnInit {
        
     }
 
+    download(){
+    var canvas = <HTMLCanvasElement>document.getElementById("mycanvas");
+    var link = document.createElement('a');
+    link.download = 'filename.png';
+    link.href = canvas.toDataURL("image/png");
+    link.click();
+    
+  }
 
 ngOnInit() {
 
-  console.log(window.event);
+var colorPicker=<HTMLCanvasElement>document.getElementById("body");
+var fillColor='rgba(0,0,0,0.1)';
+
+  colorPicker.addEventListener("change", watchColorPicker, false);
+
+function watchColorPicker(event) {
+  document.querySelectorAll("p").forEach(function(p) {
+    p.style.color = event.target.value;
+    fillColor= event.target.value;
+
+  });
+}
 
     var canvas = <HTMLCanvasElement>document.getElementById("mycanvas");
     var context = canvas.getContext("2d");
-
     
     let Dimension=25;
     let width=canvas.width;
@@ -41,14 +59,15 @@ ngOnInit() {
     for(let i=0;i<Dimension;++i){
       let x=Math.floor(i*width/Dimension);
       context.beginPath();
-      context.moveTo(x+pixelSize/2,0);
-      context.lineTo(x+pixelSize/2,height);
+      //pixelSize devide by two can 
+      context.moveTo(x+pixelSize,0);
+      context.lineTo(x+pixelSize,height);
       context.stroke();
 
       let y=Math.floor(i*height/Dimension);
       context.beginPath();
-      context.moveTo(0,y+pixelSize/2);
-      context.lineTo(width,y+pixelSize/2);
+      context.moveTo(0,y+pixelSize);
+      context.lineTo(width,y+pixelSize);
       context.stroke();
 
     }
@@ -66,7 +85,7 @@ ngOnInit() {
       }
       
       //Zoomed in red 'square'
-      context.fillStyle = "black";
+      context.fillStyle = fillColor;
       context.fillRect(pixel[0]*pixelSize, pixel[1]*pixelSize, pixelSize-1, pixelSize-1);
     });
     
