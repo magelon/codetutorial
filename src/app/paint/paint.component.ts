e329fe3554007a07bc91583a20890530956cf845
magelon <alfardproject@gmail.com>
2020-04-09 21:43:12 -0700

    c

--- src/app/paint/paint.component.ts
+++ src/app/paint/paint.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnChanges } from '@angular/core';
 
 
 @Component({
@@ -13,7 +13,13 @@ export class PaintComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngDoCheck(){
+    window.addEventListener("mouseup", (e)=>{
+      console.log("move"+e)
+    });
+  }
+
+ngOnInit() {
     var canvas = <HTMLCanvasElement>document.getElementById("mycanvas");
     var context = canvas.getContext("2d");
 
@@ -25,41 +31,53 @@ export class PaintComponent implements OnInit {
 
     context.strokeStyle='rgba(0,0,0,0.1)';
 
+  //Background
+    context.fillStyle = "white";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    //drawing the board
     for(let i=0;i<Dimension;++i){
       let x=Math.floor(i*width/Dimension);
       context.beginPath();
-      context.moveTo(x,0);
-      context.lineTo(x,height);
+      context.moveTo(x+pixelSize/2,0);
+      context.lineTo(x+pixelSize/2,height);
       context.stroke();
 
       let y=Math.floor(i*height/Dimension);
       context.beginPath();
-      context.moveTo(0,y);
-      context.lineTo(width,y);
+      context.moveTo(0,y+pixelSize/2);
+      context.lineTo(width,y+pixelSize/2);
       context.stroke();
 
     }
 
-    //Background
-    context.fillStyle = "#000";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    // canvas.addEventListener('mousedown', (e)=>{
+    //   console.log("down")
+    // });
 
-    canvas.addEventListener('click', (e)=>{
+    // canvas.addEventListener('mouseup', (e)=>{
+    //   console.log("up")
+    // });
 
-      console.log(e);
-      let offsetX=e.offsetX;
-      let offsetY=e.offsetY;
+  
 
-      let pixel=[Math.floor(offsetX/pixelSize),Math.floor(offsetY/pixelSize)];
 
-      if(e.which==1){
-          console.log(e+"mousedown")
-      }
+    // canvas.addEventListener('click', (e)=>{
+
+    //   console.log(e);
+    //   let offsetX=e.offsetX;
+    //   let offsetY=e.offsetY;
+
+    //   let pixel=[Math.floor(offsetX/pixelSize),Math.floor(offsetY/pixelSize)];
+
+    //   if(e.which==1){
+    //       console.log(e+"mousedown")
+    //   }
       
-      //Zoomed in red 'square'
-      context.fillStyle = "#FFF";
-      context.fillRect(pixel[0]*pixelSize, pixel[1]*pixelSize, pixelSize-1, pixelSize-1);
-    });
+    //   //Zoomed in red 'square'
+    //   context.fillStyle = "black";
+    //   context.fillRect(pixel[0]*pixelSize, pixel[1]*pixelSize, pixelSize-1, pixelSize-1);
+    // });
     
     
   }
